8cc22dcdc409b36ceabf11e5b1c8db656e859ec0
GiGodN <noahmjohn@gmail.com>
2019-12-08 17:15:02 -0700

    mass commit for online prep

--- index.php
+++ index.php
@@ -1,20 +1,31 @@
 <?php
     $page_title = 'Welcome';
-    $active = "home";
-    include('includes/header.html');
+    $active = "index";
+    include('./templates/Header.php');
 ?>
 
-<div class="page-header">
-    <h1>Content Header</h1>
-</div>
-<p>This is where the page-specific content goes. This section, and the corresponding header,
-    will change from one page to the next.</p>
-<p>Volutpat at varius sed sollicitudin et, arcu. Vivamus viverra. Nullam turpis. Vestibulum sed etiam. Lorem ipsum sit
-    amet dolore.
-    Nulla facilisi. Sed tortor. Aenean felis. Quisque eros. Cras lobortis commodo metus. Vestibulum vel purus.
-    In eget odio in sapien adipiscing blandit. Quisque augue tortor, facilisis sit amet, aliquam, suscipit vitae, cursussed,
-    arcu lorem ipsum dolor sit amet.</p>
+<link rel="stylesheet" href="./css/index.css">
+<div class="page-header" style="width: 100%">
+    <header class="page-header header container-fluid">
+        <div class="overlay"></div>
+        <div class="description">
+        <h1>Welcome to Team Vise!</h1>
+            <p>We are a small gaming youtube channel, (this is a work in progress website)</p>
+                <a href="https://www.youtube.com/channel/UCRyI8a523uWw1lXEjI7KRoQ" target="_blank"><button class="btn btn-outline-secondary btn-lg">Visit Our Channel</button></a>
+        </div>
+    </header>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
+    </script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"
+        integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous">
+    </script>
+    <script>
+        $(document).ready(function () {
+            $('.header').height($(window).height());
+        })
+    </script>
 
 <?php
-    include('includes/footer.html');
+    include('./templates/Footer.php');
 ?>
---
+++ templates/Header.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title><?php echo $page_title; ?></title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+        integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <?php 
+        if ($active == "index") {
+            print '<link rel="stylesheet" href="./css/include.css">';
+        }
+        else {
+            print '<link rel="stylesheet" href="../css/include.css">';
+        }
+    ?>
+</head>
+
+<body style="background-color: #555; color: #eee;">
+    <nav class="navbar navbar-expand-lg navbar-light bg-dark" style="padding-left: 5%;">
+        <a class="navbar-brand" href="#">
+            <?php 
+                if ($active == "index") {
+                    print '<img src="./images/logo.png" alt="Vise Logo" width="40" height="40">';
+                }
+                else {
+                    print '<img src="../images/logo.png" alt="Vise Logo" width="40" height="40">';
+                }
+            ?>
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item <?php if($active == 'index') {print 'active';}?>">
+                    <a class="nav-link text-light " href="/TestWeb/index.php">Home <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item <?php if($active == 'Project') {print 'active';}?>">
+                    <a class="nav-link text-light" href="/TestWeb/html/VisualProject.php">Visual Litterary Anaylisis</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle text-light" href="#" id="navbarDropdown" role="button"
+                        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Relivate Links
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" target="_blank"href="https://www.youtube.com/channel/UCRyI8a523uWw1lXEjI7KRoQ">Youtube</a>
+                        <div class="dropdown-divider"></div>
+                        <a class="dropdown-item" href="#">Twitter (WIP)</a>
+                    </div>
+                </li>
+            </ul>
+        </div>
+    </nav>
+    <div class="container">
